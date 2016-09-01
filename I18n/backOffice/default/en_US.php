0975889d0e70f896f274445fdccb94dc9251d93d
Julien Chanseaume <julien.chanseaume@gmail.com>
2016-09-01 15:02:40 +0200

    Updated translations

---
+++ I18n/backOffice/default/en_US.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'Admin Comments' => 'Admin Comments',
+    'Delete this comment' => 'Delete this comment',
+    'Edit this comment' => 'Edit this comment',
+    'Message' => 'Message',
+    'Save' => 'Save',
+);
--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -4,5 +4,6 @@ return array(
     'Admin Comments' => 'Commentaires Admin',
     'Delete this comment' => 'Supprimer le commentaire',
     'Edit this comment' => 'Modifier ce commentaire',
+    'Message' => 'Message',
     'Save' => 'Enregistrer',
 );
