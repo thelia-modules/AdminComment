1a6ef6a2493536a3255999601e908c21b52990d1
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-09 11:05:24 +0200

    feat: migrate and modernize back-office for Twig

--- Hook/BackHook.php
+++ Hook/BackHook.php
@@ -14,49 +14,112 @@
 namespace AdminComment\Hook;
 
 use AdminComment\AdminComment;
+use AdminComment\Form\AdminCommentCreateForm;
+use AdminComment\Form\AdminCommentUpdateForm;
 use AdminComment\Model\AdminCommentQuery;
 use Thelia\Core\Event\Hook\HookRenderBlockEvent;
 use Thelia\Core\Event\Hook\HookRenderEvent;
+use Thelia\Core\Form\TheliaFormFactory;
 use Thelia\Core\Hook\BaseHook;
 use Thelia\Core\Translation\Translator;
+use Symfony\Component\DependencyInjection\Attribute\Required;
 
-/**
- * Class BackHook
- * @package AdminComment\Hook
- * @author Julien Chanséaume <julien@thelia.net[>
- */
-class BackHook extends baseHook
+class BackHook extends BaseHook
 {
+    private TheliaFormFactory $formFactory;
 
-    public function onMainAfterContent(HookRenderEvent $event)
+    #[Required]
+    public function setFormFactory(TheliaFormFactory $formFactory): void
     {
+        $this->formFactory = $formFactory;
+    }
+
+    public static function getSubscribedHooks(): array
+    {
+        return [
+            'main.after-content' => [
+                ['type' => 'back', 'method' => 'onMainAfterContent'],
+            ],
+            'main.footer-js' => [
+                ['type' => 'back', 'method' => 'onMainFooterJs'],
+            ],
+            'category.edit-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'product.edit-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'folder.edit-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'content.edit-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'customer.edit-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'order.edit-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'coupon.update-js' => [
+                ['type' => 'back', 'method' => 'onEditJs'],
+            ],
+            'category.tab' => [
+                ['type' => 'back', 'method' => 'onEditTab'],
+            ],
+            'product.tab' => [
+                ['type' => 'back', 'method' => 'onEditTab'],
+            ],
+            'folder.tab' => [
+                ['type' => 'back', 'method' => 'onEditTab'],
+            ],
+            'content.tab' => [
+                ['type' => 'back', 'method' => 'onEditTab'],
+            ],
+            'order.tab' => [
+                ['type' => 'back', 'method' => 'onEditTab'],
+            ],
+            'orders.table-header' => [
+                ['type' => 'back', 'method' => 'onListHeader'],
+            ],
+            'orders.table-row' => [
+                ['type' => 'back', 'method' => 'onListRow'],
+            ],
+        ];
+    }
+
+    public function onMainAfterContent(HookRenderEvent $event): void
+    {
+        $createForm = $this->formFactory->createForm(AdminCommentCreateForm::getName());
+        $updateForm = $this->formFactory->createForm(AdminCommentUpdateForm::getName());
+
         $event->add(
-            $this->render('main-after-content.html')
+            $this->render('main-after-content.html.twig', [
+                'form' => $createForm->createView()->getView(),
+                'update_form' => $updateForm->createView()->getView(),
+            ])
         );
     }
 
-    public function onMainFooterJs(HookRenderEvent $event)
+    public function onMainFooterJs(HookRenderEvent $event): void
     {
         $event->add(
-            $this->render('main-footer-js.html')
+            $this->render('main-footer-js.html.twig')
         );
     }
 
-    public function onEditJs(HookRenderEvent $event)
+    public function onEditJs(HookRenderEvent $event): void
     {
         $params = $this->getParameters($event);
 
         if (!empty($params)) {
             $event->add(
-                $this->render(
-                    'edit-js.html',
-                    $params
-                )
+                $this->render('edit-js.html.twig', $params)
             );
         }
     }
 
-    public function onEditTab(HookRenderBlockEvent $event)
+    public function onEditTab(HookRenderBlockEvent $event): void
     {
         $params = $this->getTabParameters($event);
 
@@ -65,23 +128,27 @@ class BackHook extends baseHook
             ->filterByElementId($event->getArgument('id'))
             ->count();
 
+        $event->add([
+            'id'      => 'admin-comment',
+            'title'   => Translator::getInstance()->trans(
+                'Comment (%count)',
+                ['%count' => $count],
+                AdminComment::MESSAGE_DOMAIN
+            ),
+            'content' => '',
+        ]);
+    }
 
+    public function onListHeader(HookRenderEvent $event): void
+    {
         $event->add(
-          [
-              "id" => 'admin-comment',
-              "title" => Translator::getInstance()->trans("Comment (%count)", ['%count' => $count], AdminComment::MESSAGE_DOMAIN),
-              "content" =>  ""
-
-          ]
+            "<td class='text-center'>"
+            . Translator::getInstance()->trans('Comment', [], AdminComment::MESSAGE_DOMAIN)
+            . "</td>"
         );
     }
 
-    public function onListHeader(HookRenderEvent $event)
-    {
-        $event->add("<td class='text-center'>".Translator::getInstance()->trans('Comment', [], AdminComment::MESSAGE_DOMAIN)."</td>");
-    }
-
-    public function onListRow(HookRenderEvent $event)
+    public function onListRow(HookRenderEvent $event): void
     {
         $key = null;
 
@@ -94,21 +161,20 @@ class BackHook extends baseHook
         if ($key) {
             $count = AdminCommentQuery::create()
                 ->filterByElementKey($key)
-                ->filterByElementId($event->getArgument($key.'_id'))
+                ->filterByElementId($event->getArgument($key . '_id'))
                 ->count();
-
         }
 
-        $counter = "";
+        $counter = '';
 
         if ($count > 0) {
-            $counter = "<span class='badge' style='background-color: #f39922'>$count</span>";
+            $counter = "<span class='badge text-bg-warning'>$count</span>";
         }
 
         $event->add("<td class='text-center'>$counter</td>");
     }
 
-    protected function getParameters(HookRenderEvent $event)
+    protected function getParameters(HookRenderEvent $event): array
     {
         $out = [];
 
@@ -119,21 +185,21 @@ class BackHook extends baseHook
             'content.edit-js',
             'customer.edit-js',
             'order.edit-js',
-            'coupon.update-js'
+            'coupon.update-js',
         ];
 
         foreach ($authorizedHook as $hookName) {
             if (false !== strpos($event->getCode(), $hookName)) {
                 $key = explode('.', $hookName)[0];
-                $id = (int)$event->getArgument($key . '_id');
+                $id = (int) $event->getArgument($key . '_id');
                 // try to get from url
                 if (0 === $id) {
-                    $id = (int)$this->getRequest()->query->get($key . '_id', 0);
+                    $id = (int) $this->getRequest()->query->get($key . '_id', 0);
                 }
                 if (0 !== $id) {
                     $out = [
                         'key' => $key,
-                        'id' => $id
+                        'id'  => $id,
                     ];
                 }
                 break;
@@ -143,7 +209,7 @@ class BackHook extends baseHook
         return $out;
     }
 
-    protected function getTabParameters(HookRenderBlockEvent $event)
+    protected function getTabParameters(HookRenderBlockEvent $event): array
     {
         $out = [];
 
@@ -152,7 +218,7 @@ class BackHook extends baseHook
             'product.tab',
             'folder.tab',
             'content.tab',
-            'order.tab'
+            'order.tab',
         ];
 
         foreach ($authorizedHook as $hookName) {
