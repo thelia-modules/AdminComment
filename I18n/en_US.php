deaa7510332d8c5507ca9fa4da39658d9fd27613
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-29 09:40:31 +0200

    fix: add French translation for the module title and description

--- I18n/en_US.php
+++ I18n/en_US.php
@@ -1,10 +1,12 @@
 <?php
 
 return array(
+    'Admin Comments' => 'Admin Comments',
     'Admin Id' => 'Admin Id',
     'Comment' => 'Comment',
     'Comment Id' => 'Comment Id',
     'Element id' => 'Element id',
     'Element key' => 'Element key',
+    'This module lets administrators add private comments on catalog and order pages. There is no configuration: a "Comment" tab and an inline comment widget are automatically added to the edit pages of products, categories, contents, folders, customers, orders and coupons.' => 'This module lets administrators add private comments on catalog and order pages. There is no configuration: a "Comment" tab and an inline comment widget are automatically added to the edit pages of products, categories, contents, folders, customers, orders and coupons.',
     'Unknown comment' => 'Unknown comment',
 );
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,11 +1,13 @@
 <?php
 
 return array(
+    'Admin Comments' => 'Commentaires Admin',
     'Admin Id' => 'Id Admin',
     'Comment' => 'Commentaire',
     'Comment (%count)' => 'Commentaires (%count)',
     'Comment Id' => 'Id commentaire',
     'Element id' => 'Id de l\'élément',
     'Element key' => 'Clé de l\'élément',
+    'This module lets administrators add private comments on catalog and order pages. There is no configuration: a "Comment" tab and an inline comment widget are automatically added to the edit pages of products, categories, contents, folders, customers, orders and coupons.' => 'Ce module permet aux administrateurs d\'ajouter des commentaires privés sur les pages du catalogue et des commandes. Aucune configuration n\'est nécessaire : un onglet « Commentaire » et un champ de commentaire intégré sont automatiquement ajoutés aux pages d\'édition des produits, catégories, contenus, dossiers, clients, commandes et codes promo.',
     'Unknown comment' => 'Commentaire inconnu',
 );
